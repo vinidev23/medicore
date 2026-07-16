797249ecdfa0f4aaf684b2774bb39540878bf9dd
vinidev23 <vinidev23>
2026-07-16 16:15:45 -0300

    feat: criando tabelas

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://engclin_user:engclin_pass@localhost:5432/engclin_db",
+    "postgresql://medicore:engclin_pass@localhost:5432/medicore",
 )
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"client_encoding": "utf8"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
